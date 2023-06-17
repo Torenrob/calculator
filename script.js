9213d47d569f4bd0187e63d741020a4c94442647
TorenR <torentremaine@outlook.com>
2023-06-17 03:30:27 -0400

    feat: add prelim js functs for calcs not yet wrkin

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+// DOM Elements
 let calcTopDisplay = document.getElementById("topDisplay");
 calcMainDisplay = document.getElementById("mainDisplay");
 calcBtns = Array.from(document.getElementsByClassName("btns"));
 
+// Variables
+let entry = "";
+let result = "";
+
+// DOM Manipulation (Listenters & Dynamic Changes)
 calcBtns.forEach((element) => {
 	element.addEventListener("click", (btn) => {
-		console.log(btn.target);
+		if (btn.target.innerText == "ce") {
+			entry = "";
+		} else if (btn.target.innerText == "=") {
+			console.log("equal");
+		} else {
+			entry += `${btn.target.innerText}`;
+		}
+		calcTopDisplay.innerText = `${entry}`;
 	});
 });
+
+// functions
+function getResult(entry) {}

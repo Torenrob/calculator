00f12b67853764dc63e3f94f1e32a02b20804f7a
TorenR <torentremaine@outlook.com>
2023-06-18 13:22:33 -0400

    feat: create js for calc btns in progress

--- script.js
+++ script.js
@@ -10,16 +10,31 @@ let result = "";
 // DOM Manipulation (Listenters & Dynamic Changes)
 calcBtns.forEach((element) => {
 	element.addEventListener("click", (btn) => {
-		if (btn.target.innerText == "ce") {
+		let usrInput = btn.target.innerText;
+		if (usrInput == "ce") {
 			entry = "";
-		} else if (btn.target.innerText == "=") {
+		} else if (usrInput == "=") {
 			console.log("equal");
 		} else {
-			entry += `${btn.target.innerText}`;
+			if (/[0-9]/.test(usrInput)) {
+				entry += `${usrInput}`;
+			} else if (/\./.test(usrInput)) {
+				entry = noDblops(entry);
+			} else {
+				entry += ` ${usrInput}`;
+				entry = noDblops(entry);
+				entry += " ";
+			}
 		}
 		calcTopDisplay.innerText = `${entry}`;
 	});
 });
 
 // functions
-function getResult(entry) {}
+function noDblops(string) {
+	str = string.replaceAll(" ", "");
+	str = str.substring(str.length - 2);
+	if (/[^0-9]{2}/.search(str)) {
+		return string.replace(string.length - 1, "");
+	}
+}
